2949184997edb0974b601aa22b6e345502d739af
Sergey-Proskurnin <proskurnin@ua.fm>
2023-05-24 20:42:55 +0300

    Adds kata Write Number in Expanded Form 6kyu

---
+++ js/Write Number in Expanded Form-6kyu.js
@@ -0,0 +1,26 @@
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it
+// as a string in Expanded Form.For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+function expandedForm(num) {
+  return num
+    .toString()
+    .split('')
+    .reduce((acc, el, i, arr) => {
+      if (el !== '0') {
+        acc.push(el + '0'.repeat(arr.length - 1 - i));
+      }
+      return acc;
+    }, [])
+    .join(' + ');
+}
+
+console.log(expandedForm(12)); // Should return '10 + 2'
+console.log(expandedForm(42)); // Should return '40 + 2'
+console.log(expandedForm(70304)); // Should return '70000 + 300 + 4'
